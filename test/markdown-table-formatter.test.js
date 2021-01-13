9bfa28b2c06c9e9cf18bb9f55507227791ada822
NicolasVuillamy <nicolas.vuillamy@gmail.com>
2021-01-13 19:06:33 +0100

    fixes

--- test/markdown-table-formatter.test.js
+++ test/markdown-table-formatter.test.js
@@ -8,13 +8,14 @@ const {
     checkStdOutIncludes,
     checkStdErrIncludes */
 } = require("./helpers/common");
-//const { MarkdownTableFormatterCli } = require('../lib/cli');
+const { MarkdownTableFormatterCli } = require('../lib/index');
 
 describe("Tests", () => {
     beforeEach(beforeEachTestCase);
 
     it("should format a single file", async () => {
-
+        const args = [null, null, "test_files/markdown_bad_1.md"];
+        await new MarkdownTableFormatterCli().run(args);
     });
 
     it("should format multiple files", async () => {
