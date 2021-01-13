074b7ebc5babcccfa8680fbb49b3540feb171fa8
NicolasVuillamy <nicolas.vuillamy@gmail.com>
2021-01-13 17:20:38 +0100

    Initial version

---
+++ test/markdown-table-formatter.test.js
@@ -0,0 +1,46 @@
+#! /usr/bin/env node
+"use strict";
+//const { MarkdownTableFormatter } = require('../lib/index');
+
+const {
+    beforeEachTestCase,
+/*    checkStatus,
+    checkStdOutIncludes,
+    checkStdErrIncludes */
+} = require("./helpers/common");
+//const { MarkdownTableFormatterCli } = require('../lib/cli');
+
+describe("Tests", () => {
+    beforeEach(beforeEachTestCase);
+
+    it("should format a single file", async () => {
+
+    });
+
+    it("should format multiple files", async () => {
+
+    });
+
+    it("should check a single file with successs", async () => {
+
+    });
+
+    it("should check a single file with failure", async () => {
+
+    });
+
+    it("should tell that a file argument is mandatory", async () => {
+
+    });
+
+    it("should show version", async () => {
+
+    });
+
+    it("should show help", async () => {
+
+    });
+
+
+});
+
