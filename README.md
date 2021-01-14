bd7ee3e2bd3de9d13b9b6a64f8f63f7b23332725
Nicolas Vuillamy <nicolas.vuillamy@gmail.com>
2021-01-14 12:52:55 +0100

    Fix formatter and test classes (#3)
    * tests
    
    * fix test classes
    
    * check with insiders
    
    * cspell
    
    * [Mega-Linter] Apply linters fixes
    
    Co-authored-by: nvuillam <nvuillam@users.noreply.github.com>

--- README.md
+++ README.md
@@ -79,6 +79,10 @@ Please follow [Contribution instructions](https://github.com/nvuillam/markdown-t
 
 ## RELEASE NOTES
 
+## [1.0.0] 2021-01-13
+
+- First working version :)
+
 ## [0.0.1] 2021-01-13
 
 - Initial version
--- test/markdown-table-formatter.test.js
+++ test/markdown-table-formatter.test.js
@@ -1,14 +1,16 @@
 #! /usr/bin/env node
 "use strict";
 const assert = require("assert");
+const os = require("os");
+const fse = require("fs-extra");
+const path = require("path");
 const { MarkdownTableFormatterCli } = require('../lib/index');
 const {
     beforeEachTestCase,
     TEST_FILE_GOOD,
     TEST_FILE_BAD,
     checkStatus,
-/*    checkStdOutIncludes,
-    checkStdErrIncludes */
+    checkStdOutIncludes
 } = require("./helpers/common");
 
 describe("Tests", () => {
@@ -27,32 +29,47 @@ describe("Tests", () => {
     });
 
     it("should format a single file", async () => {
-        const args = [null, null, TEST_FILE_BAD];
+        const file_bad_tmp = os.tmpdir() +"/"+ TEST_FILE_BAD;
+        fse.copySync(path.resolve(TEST_FILE_BAD), file_bad_tmp);
+        const args = [null, null, file_bad_tmp];
         const res = await new MarkdownTableFormatterCli().run(args);
         checkStatus(res.status,0)
         assert(res.updates.length === 1,`${TEST_FILE_BAD} should have been updated`)
     });
 
     it("should format multiple files", async () => {
-        const args = [null, null,TEST_FILE_GOOD, TEST_FILE_BAD];
+        const file_bad_tmp = os.tmpdir() +"/"+ TEST_FILE_BAD;
+        fse.copySync(path.resolve(TEST_FILE_BAD), file_bad_tmp);
+        const file_bad_tmp2 = os.tmpdir() +"/x/"+ TEST_FILE_BAD;
+        fse.copySync(path.resolve(TEST_FILE_BAD), file_bad_tmp2);
+        const args = [null, null,file_bad_tmp, file_bad_tmp2];
         const res = await new MarkdownTableFormatterCli().run(args);
         checkStatus(res.status,0)
-        assert(res.updates.length === 2,`${TEST_FILE_BAD} should have been updated`)
+        assert(res.updates.length === 2,`${file_bad_tmp} and ${file_bad_tmp2} should have been updated`)
     });
 
-    /*
+    
     it("should tell that a file argument is mandatory", async () => {
-
+        const args = [null, null];
+        try {
+            await new MarkdownTableFormatterCli().run(args);
+        } catch (e) {
+            checkStdOutIncludes("You must send a file or a list of files as argument",e.message,"")            
+        }
     });
 
     it("should show version", async () => {
-
+        const args = [null, null, "--version"];
+        const res = await new MarkdownTableFormatterCli().run(args);
+        checkStdOutIncludes("markdown-table-formatter version",res.stdout,"")
     });
 
     it("should show help", async () => {
-
+        const args = [null, null, "--help"];
+        const res = await new MarkdownTableFormatterCli().run(args);
+        checkStdOutIncludes("markdown-table-formatter [options]",res.stdout,"")
     });
-    */
+    
 
 
 });
